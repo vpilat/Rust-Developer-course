4dd50676e6f33d0051d280e918a787906bfe1618
Vladimír Pilát <18121585+vpilat@users.noreply.github.com>
2023-10-09 11:41:48 +0200

    homework 2

---
+++ homework_2/target/debug/deps/deunicode-a1575a13c758098c.d
@@ -0,0 +1,7 @@
+/Users/vladimirpilat/Documents/GitHub/Rust-Developer-course/homework_2/target/debug/deps/deunicode-a1575a13c758098c.rmeta: /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/lib.rs /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/mapping.txt /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/pointers.bin
+
+/Users/vladimirpilat/Documents/GitHub/Rust-Developer-course/homework_2/target/debug/deps/deunicode-a1575a13c758098c.d: /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/lib.rs /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/mapping.txt /Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/pointers.bin
+
+/Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/lib.rs:
+/Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/mapping.txt:
+/Users/vladimirpilat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/deunicode-0.4.4/src/pointers.bin:
---
+++ homework_2/target/debug/deps/homework_2-7af935d63b6fa775.d
@@ -0,0 +1,5 @@
+/Users/vladimirpilat/Documents/GitHub/Rust-Developer-course/homework_2/target/debug/deps/homework_2-7af935d63b6fa775.rmeta: src/main.rs
+
+/Users/vladimirpilat/Documents/GitHub/Rust-Developer-course/homework_2/target/debug/deps/homework_2-7af935d63b6fa775.d: src/main.rs
+
+src/main.rs:
