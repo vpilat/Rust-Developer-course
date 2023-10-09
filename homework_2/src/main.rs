1ae10dc47b65e31db149257b04dc6b99eb50e0ca
Vladimír Pilát <18121585+vpilat@users.noreply.github.com>
2023-10-09 20:53:58 +0200

    homework

--- homework_2/src/main.rs
+++ homework_2/src/main.rs
@@ -1,23 +1,24 @@
 //! Homework from Lesson 2
 /// Rust Programming Code Example
 
-use std::io::stdin;
-// extern crate slug;
-// use slug::slugify;
+use std::io;
+extern crate slug;
+use slug::slugify;
 
 fn main() {
-    println!();
-    print!("Enter text: ");
-    
-    // Get the text from the keyboard
-    let mut line = String::new();
-    stdin().read_line(&mut line).unwrap();
+    // Create a new instance of stdin
+    let stdin = io::stdin();
 
-    // Slugify the text.
-    // let slug = text.trim().to_lowercase().replace(" ", "-");
+    // Create a mutable string to store the input
+    let mut input = String::new();
 
-    // Print the slug to the console.
-    println!();
-    println!("{}", line);
-    println!();
+    // Read a line from stdin and store it in the 'input' string
+    stdin
+        .read_line(&mut input)
+        .expect("Failed to read line from stdin");
+
+    let slug = slugify("Hello world");
+
+    // Print the line back out
+    println!("You entered: {}", input);
 }
