5acf409d690c5ff320f22b7d197e1db546b42957
Vladimír Pilát <18121585+vpilat@users.noreply.github.com>
2023-10-09 13:46:30 +0200

    homework

--- homework_2/src/main.rs
+++ homework_2/src/main.rs
@@ -1,8 +1,23 @@
-extern crate slug;
-use slug::slugify;
+//! Homework from Lesson 2
+/// Rust Programming Code Example
+
+use std::io::stdin;
+// extern crate slug;
+// use slug::slugify;
 
 fn main() {
-    println!("Hello, world!");
+    println!();
+    print!("Enter text: ");
+    
+    // Get the text from the keyboard
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap();
 
-    let slug = slugify("Hello world");
+    // Slugify the text.
+    // let slug = text.trim().to_lowercase().replace(" ", "-");
+
+    // Print the slug to the console.
+    println!();
+    println!("{}", line);
+    println!();
 }
