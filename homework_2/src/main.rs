b03f371a2fedaa344cb6ed83adbddd786b3509f6
Vladimír Pilát <18121585+vpilat@users.noreply.github.com>
2023-10-15 19:46:30 +0200

    Homework

--- homework_2/src/main.rs
+++ homework_2/src/main.rs
@@ -1,4 +1,10 @@
 //! Homework from Lesson 2 - Rust Basics: Syntax and Variables
+//! 
+//! use: CLI param  lowercase - convert string to lowercase
+//!                 upercase  - convert string to upercase
+//!                 no_spaces - convert string to string with no spaces
+//!                 slugify   - convert string to lowercase with - instead of spaces
+//! 
 /// Rust Programming Code Example
 
 use std::io;
@@ -7,15 +13,25 @@ extern crate slug;
 use slug::slugify;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{}", args[0]);
+    let args: Vec<String> = env::args().collect();                                      // init var, store cli args
 
-    let mut input = String::new();
-    println!("Enter string please...");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("arg: {}", args[1]);                                                       // print cli args
 
-    println!("Original: {}", input);
+    let mut input = String::new();                                              // init var
+    
+    println!("Enter source text, please...");                                           // print message (ask for string)
 
-    let slug = slugify(input);
-    println!("Slugified: {}", slug);
+    io::stdin().read_line(&mut input).expect("Failed to read text");           // read string from stdio
+
+    print!("Input : {}", input);                                                        // print input string
+
+    print!("Output: ");
+
+    match args[1].as_str() {                                                            // match string manipulations method and print manipulated string
+        "lowercase" => println!("{}", input.to_lowercase()),
+        "upercase" => println!("{}", input.to_uppercase()),
+        "no_spaces" => println!("{}", input.replace(" ", "")),
+        "slugify" => println!("{}", slugify(input)),
+        _ => println!("unsupported method..."),
+    }
 }
